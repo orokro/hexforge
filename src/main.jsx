1d7f6f250f70a6bb9f6df39815b666026b34ea77
Greg Miller <hypurban86@gmail.com>
2025-11-12 23:50:11 -0500

    formatting

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,22 @@
+/*
+	main.jsx
+	--------
+
+	Main bootstrap file for a React application using Vite.
+*/
+
+// react imports
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+
+// styles
 import './index.css'
+
+// component imports
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+	<StrictMode>
+		<App />
+	</StrictMode>,
 )
