4bf75231ac90b2dd86425a728175d8268d5c04cc
Greg Miller <hypurban86@gmail.com>
2025-11-13 16:04:19 -0500

    Adds empty scaffold for project

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+/*
+	vite.config.js
+	--------------
+
+	Set up Vite configuration for a React project with Emotion support.
+*/
+
+// imports
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+	plugins: [
+		react({
+			// enables Emotion's css prop, better labels, source maps
+			babel: { plugins: ["@emotion"] }
+		})
+	]
+});
