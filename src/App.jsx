de03f600f556c56ea0ef3ee89d81cf621b159b8c
Greg Miller <hypurban86@gmail.com>
2025-11-16 21:51:25 -0800

    RootLayout component

--- src/App.jsx
+++ src/App.jsx
@@ -5,17 +5,25 @@
 	Bootstraps the editor: UI layout + R3F scene root.
 */
 
-// imports
+// react imports
 import React from "react";
+/** @jsxImportSource @emotion/react */
+import { css } from "@emotion/react";
+
+// component imports
+import { RootLayout } from "./components/ui/RootLayout";
 
 // constants and globals
 // (none)
 
 const App = () => {
 	return (
-		<div data-ui="App" className="app">
-			hi
-			<style>{css}</style>
+		<div 
+			data-ui="App"
+			className="app"
+			css={style}
+		>
+			<RootLayout>hi hi</RootLayout>
 		</div>
 	);
 };
@@ -23,9 +31,11 @@ const App = () => {
 export default App;
 
 // 100% of styles here defined in one big CSS block, for all the parts of the template
-const css = `
+const style = css`
+
 	[data-ui="App"].app{
 		position: fixed;
 		inset: 0;
 	}
-`
+`;
+
